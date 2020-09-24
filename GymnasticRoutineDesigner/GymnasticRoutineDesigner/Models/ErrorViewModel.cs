c180419a8791ff8ef785ffc82a4500e4beb1b2cc
Max Dussen <414805@student.fontys.nl>
2020-09-24 11:38:31 +0200

    Project setup

---
+++ GymnasticRoutineDesigner/GymnasticRoutineDesigner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GymnasticRoutineDesigner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
