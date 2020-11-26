473b86fb2ec8d3323f23c819296ffc3ad5fd8ab6
Max Dussen <414805@student.fontys.nl>
2020-11-26 12:56:07 +0100

    Failing test

--- GymnasticRoutineDesigner/RoutineDesignerTests/Tests/ApparatusTests.cs
+++ GymnasticRoutineDesigner/RoutineDesignerTests/Tests/ApparatusTests.cs
@@ -27,7 +27,7 @@ namespace RoutineDesignerTests.Tests
 
             Apparatus app = Repo.GetById(Id);
 
-            Assert.AreEqual(Id, app.Id);
+            Assert.AreNotEqual(Id, app.Id);
         }
 
         [TestMethod]
